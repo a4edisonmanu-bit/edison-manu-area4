a127336f39ef68f5638d7e637191a9c7f23641e6
a4edisonmanu-bit <a4edisonmanu@gmail.com>
2026-06-18 15:49:43 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = "manu-area4-v1";
+
+self.addEventListener("install", event => {
+  console.log("Service Worker installato");
+});
+
+self.addEventListener("fetch", event => {
+});
